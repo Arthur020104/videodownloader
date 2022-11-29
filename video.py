f5c93cda6e245fef401a2a8983f106425bbcb3e9
Arthur <arthur020104@gmail.com>
2022-11-29 14:30:03 -0300

    fazendo alteracoes no codigo do videodownloader para baixar melhor versao do video ou do audio

--- video.py
+++ video.py
@@ -1,21 +1,66 @@
 from pytube import YouTube
+import os
+import subprocess
 def main():
-    y = 0
-    x = int(input("Audio(1:sim,0:nao):"))
-    audioo = '' if 0 == x else 'progressive="True"'
+    audio = 0
+    video = 0
+    audioo = '' 
     qual = []
+    qualaudio = []
     link  = input("Link do video:")
-    for x in YouTube(link).streams:
+    filename = input("Nome do video final:")
+    onlyaudio = int(input("Apenas audio digite 1, caso contrário digite qualquer número: ")) 
+    linkstream = YouTube(link).streams
+    for x in linkstream:
         z = str(x)
-        l = int(retornarpesquisa(z,'res="',audioo))
-        qual.append( l)
-    for x in YouTube(link).streams:
+        try :
+            l = int(retornarpesquisa(z,'res="',audioo))
+            qual.append( l)
+        except:
+            pass
+    for x in linkstream:
+        z = str(x)
+        try :
+            l = int(retornarpesquisa(z,'abr="','type="audio"'))
+            qualaudio.append(l)
+        except:
+            pass
+    for x in linkstream:
         z = str(x)
         k = max(qual)
-        if str(k) in z :
-            y=x
+        if (str(k)+'p') in z :
+            video=x
             break
-    y.download('C:\\Users\\Arthur\\Videos')
+    for x in linkstream:
+        z = str(x)
+        k = max(qualaudio)
+        if (str(k)+'kbps') in z :
+            audio=x
+            break
+    if onlyaudio == 1:
+        audiotype = (retornarpesquisa(str(audio), 'mime_type="audio/','XXXXXX'))
+        audiotype= audiotype[0:audiotype.find('"')]
+        audio.download('C:\\Users\\Arthur\\Videos',filename = filename+'.'+audiotype)
+        if audiotype != 'mp3':
+            src = ('C:\\Users\\Arthur\\Videos\\'+filename+'.'+audiotype)
+            dst = ('C:\\Users\\Arthur\\Videos\\'+filename+'.mp3')
+            print(subprocess.run(f'ffmpeg -i "{src}" "{dst}"',shell=True,capture_output=True))
+            os.remove('C:/Users/Arthur/Videos/'+filename+'.'+audiotype)
+    else:
+        audiotype = (retornarpesquisa(str(audio), 'mime_type="audio/','XXXXXX'))
+        audiotype= audiotype[0:audiotype.find('"')]
+        videoptype = (retornarpesquisa(str(video), 'mime_type="video/','XXXXXX'))
+        videoptype= videoptype[0:videoptype.find('"')]
+        audio.download('C:\\Users\\Arthur\\Videos',filename = filename+'propad.'+audiotype)
+        video.download('C:\\Users\\Arthur\\Videos',filename = filename+'prop.'+videoptype)
+        import moviepy.editor as mpe
+        #source::https://www.programcreek.com/python/example/105718/moviepy.editor.VideoFileClip Example no 6 ||https://stackoverflow.com/questions/28219049/combining-an-audio-file-with-video-file-in-python
+        my_clip = mpe.VideoFileClip('C:/Users/Arthur/Videos/'+filename+'prop.'+videoptype)
+        audio_background = mpe.AudioFileClip('C:/Users/Arthur/Videos/'+filename+'propad.'+audiotype)
+        final_clip = my_clip.set_audio(audio_background)
+        print(final_clip.write_videofile('C:/Users/Arthur/Videos/'+filename+'.mp4'))
+        os.remove('C:/Users/Arthur/Videos/'+filename+'prop.'+videoptype)
+        os.remove('C:/Users/Arthur/Videos/'+filename+'propad.'+audiotype)
 def retornarpesquisa(frase, acao,audioo):
     location = frase.find(acao)
     location += len(acao)
@@ -23,8 +68,9 @@ def retornarpesquisa(frase, acao,audioo):
     name = "".join([i if i != "/n" and i != ""else "" for i in frase[location:len(frase)]])
     if audioo in name:
         name_t = ''
-        for x in name:
-            if x == 'p':
+        for i in range(len(name)):
+            x=name[i]
+            if x =='p'or (x=='k'and name[i+1]=='b'):
                 break
             name_t += x
         try:
@@ -32,7 +78,7 @@ def retornarpesquisa(frase, acao,audioo):
                 return name_t.lstrip()
         except ValueError: 
             return 1
-    return 1
+    return name
 
 if __name__=='__main__':
     main()
